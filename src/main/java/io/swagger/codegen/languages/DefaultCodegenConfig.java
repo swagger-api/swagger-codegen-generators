1aaa87203d64ca59675e660069d09ea599bda86c
Jeremie Bresson <dev@jmini.fr>
2018-03-07 14:02:08 +0100

    Remove jmustache dependency

--- src/main/java/io/swagger/codegen/languages/DefaultCodegenConfig.java
+++ src/main/java/io/swagger/codegen/languages/DefaultCodegenConfig.java
@@ -2,7 +2,6 @@ package io.swagger.codegen.languages;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.github.jknack.handlebars.Handlebars;
-import com.samskivert.mustache.Mustache;
 import io.swagger.codegen.CliOption;
 import io.swagger.codegen.CodegenArgument;
 import io.swagger.codegen.CodegenConfig;
@@ -367,12 +366,6 @@ public abstract class DefaultCodegenConfig implements CodegenConfig {
 
     @Override
     public void processOpenAPI(OpenAPI openAPI) {
-    }
-
-    // override with any special handling of the JMustache compiler
-    @SuppressWarnings("unused")
-    public Mustache.Compiler processCompiler(Mustache.Compiler compiler) {
-        return compiler;
     }
 
     // override with any special text escaping logic
