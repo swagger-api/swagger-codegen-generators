ce3dc45782406d694cf849486aca15474230aef3
Hugo Mercado <hugo.mario.mercado@gmail.com>
2018-09-03 05:46:41 -0500

    added helper to check if a collection is empty.

---
+++ src/main/java/io/swagger/codegen/v3/generators/handlebars/NotEmptyHelper.java
@@ -0,0 +1,26 @@
+package io.swagger.codegen.v3.generators.handlebars;
+
+import com.github.jknack.handlebars.Helper;
+import com.github.jknack.handlebars.Options;
+
+import java.io.IOException;
+import java.util.Collection;
+
+public class NotEmptyHelper implements Helper<Collection> {
+    public static final String NAME = "notEmpty";
+
+    @Override
+    public Object apply(Collection collection, Options options) throws IOException {
+        final Options.Buffer buffer = options.buffer();
+
+        System.out.println("------------------------ collection:");
+        System.out.println(collection);
+
+        if (collection == null || collection.isEmpty()) {
+            buffer.append(options.inverse());
+        } else {
+            buffer.append(options.fn());
+        }
+        return buffer;
+    }
+}
