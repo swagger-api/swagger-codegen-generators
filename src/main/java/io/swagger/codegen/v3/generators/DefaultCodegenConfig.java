a06fe5ed945686e77760c2efc7782d0a383c1984
Michael P Davis <michael.p.davis@workiva.com>
2019-12-18 15:05:38 -0600

    Handle default parameter values

--- src/main/java/io/swagger/codegen/v3/generators/DefaultCodegenConfig.java
+++ src/main/java/io/swagger/codegen/v3/generators/DefaultCodegenConfig.java
@@ -2376,6 +2376,19 @@ public abstract class DefaultCodegenConfig implements CodegenConfig {
             LOGGER.info("working on Parameter " + parameter.getName());
         }
 
+        // move the defaultValue for headers and query params
+        if (parameter instanceof QueryParameter) {
+            QueryParameter qp = (QueryParameter) parameter;
+            if(qp.getSchema().getDefault() != null) {
+                codegenParameter.defaultValue = qp.getSchema().getDefault().toString();
+            }
+        } else if (parameter instanceof HeaderParameter) {
+            HeaderParameter hp = (HeaderParameter) parameter;
+            if(hp.getSchema().getDefault() != null) {
+                codegenParameter.defaultValue = hp.getSchema().getDefault().toString();
+            }
+        }
+
         if (parameter.getExtensions() != null && !parameter.getExtensions().isEmpty()) {
             codegenParameter.vendorExtensions.putAll(parameter.getExtensions());
         }
