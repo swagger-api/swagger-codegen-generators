02d23e588a56d6b4f4c45d8be11ee0aeed24481a
HugoMario <hugo.mario.mercado@gmail.com>
2020-01-29 08:33:37 -0500

    fixed java model test.

--- src/test/java/io/swagger/codegen/v3/generators/java/JavaModelTest.java
+++ src/test/java/io/swagger/codegen/v3/generators/java/JavaModelTest.java
@@ -1044,7 +1044,7 @@ public class JavaModelTest extends AbstractCodegenTest {
 
         final CodegenConfigurator configurator = new CodegenConfigurator()
                 .setLang("java")
-                .setInputSpec("src/test/resources/3_0_0/petstore.json")
+                .setInputSpecURL("src/test/resources/3_0_0/petstore.json")
                 .setOutputDir(output.getAbsolutePath());
 
         final ClientOptInput clientOptInput = configurator.toClientOptInput();
