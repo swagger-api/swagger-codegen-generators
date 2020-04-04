0b0496846f4000ddc21c3cf035bd3dc1599eb3c7
HugoMario <hugo.mario.mercado@gmail.com>
2020-04-04 06:10:26 -0500

    map big decimals

--- src/main/java/io/swagger/codegen/v3/generators/go/AbstractGoCodegen.java
+++ src/main/java/io/swagger/codegen/v3/generators/go/AbstractGoCodegen.java
@@ -78,6 +78,7 @@ public abstract class AbstractGoCodegen extends DefaultCodegenConfig {
         typeMapping.put("number", "float32");
         typeMapping.put("float", "float32");
         typeMapping.put("double", "float64");
+        typeMapping.put("BigDecimal", "float64");
         typeMapping.put("boolean", "bool");
         typeMapping.put("string", "string");
         typeMapping.put("UUID", "string");
