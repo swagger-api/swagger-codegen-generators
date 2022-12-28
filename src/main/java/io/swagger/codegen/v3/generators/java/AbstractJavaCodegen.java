80b17514b7b2cd8fa021579be0cae21530b9be8d
Fyro <Fyro-Ing@users.noreply.github.com>
2022-12-28 20:32:22 +0100

    fix

--- src/main/java/io/swagger/codegen/v3/generators/java/AbstractJavaCodegen.java
+++ src/main/java/io/swagger/codegen/v3/generators/java/AbstractJavaCodegen.java
@@ -1,5 +1,10 @@
 package io.swagger.codegen.v3.generators.java;
 
+import static io.swagger.codegen.v3.CodegenConstants.HAS_ENUMS_EXT_NAME;
+import static io.swagger.codegen.v3.CodegenConstants.IS_ENUM_EXT_NAME;
+import static io.swagger.codegen.v3.generators.features.NotNullAnnotationFeatures.NOT_NULL_JACKSON_ANNOTATION;
+import static io.swagger.codegen.v3.generators.handlebars.ExtensionHelper.getBooleanValue;
+
 import com.github.jknack.handlebars.Handlebars;
 import io.swagger.codegen.v3.CliOption;
 import io.swagger.codegen.v3.CodegenArgument;
@@ -26,11 +31,6 @@ import io.swagger.v3.oas.models.media.StringSchema;
 import io.swagger.v3.oas.models.parameters.RequestBody;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.parser.util.SchemaTypeUtil;
-import org.apache.commons.lang3.BooleanUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.File;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -42,11 +42,10 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.regex.Pattern;
-
-import static io.swagger.codegen.v3.CodegenConstants.HAS_ENUMS_EXT_NAME;
-import static io.swagger.codegen.v3.CodegenConstants.IS_ENUM_EXT_NAME;
-import static io.swagger.codegen.v3.generators.features.NotNullAnnotationFeatures.NOT_NULL_JACKSON_ANNOTATION;
-import static io.swagger.codegen.v3.generators.handlebars.ExtensionHelper.getBooleanValue;
+import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class AbstractJavaCodegen extends DefaultCodegenConfig {
     private static Logger LOGGER = LoggerFactory.getLogger(AbstractJavaCodegen.class);
@@ -503,8 +502,10 @@ public abstract class AbstractJavaCodegen extends DefaultCodegenConfig {
             additionalProperties.put("legacyDates", true);
         }
 
-        setJakarta(Boolean.parseBoolean(String.valueOf(additionalProperties.get(JAKARTA))));
-        additionalProperties.put(JAKARTA, jakarta);
+        if (additionalProperties.containsKey(JAKARTA)) {
+            setJakarta(Boolean.parseBoolean(String.valueOf(additionalProperties.get(JAKARTA))));
+            additionalProperties.put(JAKARTA, jakarta);
+        }
     }
 
     private void sanitizeConfig() {
